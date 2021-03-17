b59afd3420b1b43d6fa8317c3776a37b8ed72355
Ikraam <IbnDaanis@Gmail.com>
2021-03-17 02:47:56 +0000

    added more operations

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,118 @@
 class Calculator {
-  current: number
-  previous: number
+  current: string = '0'
+  previous: string
+  operation: string
 
   public reset(): void {
-    this.current = null
+    this.current = '0'
+    this.previous = null
+    this.operation = null
+  }
+
+  public setInput(input: string | number) {
+    const value =
+      this.current === '0' ? input.toString() : this.current.toString() + input.toString()
+    this.setCurrent(value)
+  }
+
+  public setCurrent(input: string | number) {
+    console.log('Input: ', input)
+    this.current = input.toString()
+  }
+
+  public setPrevious(input: string | number) {
+    this.previous = input.toString()
+  }
+
+  public setOperation(input: string) {
+    this.operation = input
+  }
+
+  public divide() {
+    this.current = (+this.previous / +this.current).toString()
     this.previous = null
   }
 
-  public setCurrent(input: number) {
-    this.previous = this.current
-    this.current = input
-    console.log(this.current, this.previous)
-    return this
+  public multiply() {
+    this.current = (+this.previous * +this.current).toString()
+    this.previous = null
   }
 
   public add() {
-    this.current = this.current + this.previous
+    this.current = (+this.previous + +this.current).toString()
     this.previous = null
-    return this
+  }
+
+  public subtract() {
+    this.current = (+this.previous - +this.current).toString()
+    this.previous = null
   }
 }
 
 const calculator = new Calculator()
 
 const DOM_EVENTS = (() => {
-  const digits = document.querySelectorAll('[data-digit]')
-  const operators = document.querySelectorAll('[data-operator]')
+  const digits: NodeListOf<HTMLButtonElement> = document.querySelectorAll('[data-digit]')
+  const operators: NodeListOf<HTMLButtonElement> = document.querySelectorAll('[data-operator]')
   const equals = document.querySelector('[data-equals]')
   const clearBtn = document.querySelector('[data-clear]')
   const negOrPos = document.querySelector('[data-negOrPos]')
   const percent = document.querySelector('[data-percent]')
   const display = document.querySelector('#displayText')
 
-  const updateDisplay = () => {
+  const updateDisplay = (): void => {
     display.textContent = calculator.current.toString()
   }
 
+  const calculate = (): void => {
+    console.log('Calculate', calculator.current, calculator.previous)
+    // switch (calculator.operation) {
+    //   case '/':
+    //     calculator.divide()
+    //     break
+    //   case '*':
+    //     calculator.multiply()
+    //     break
+    //   case '+':
+    //     calculator.add()
+    //     break
+    //   case '-':
+    //     calculator.subtract()
+    //     break
+    //   default:
+    //     return
+    // }
+  }
+
+  updateDisplay()
+
   digits.forEach(digit => {
     digit.addEventListener('click', (e: Event) => {
-      display.textContent = digit.textContent
-
-      console.log()
+      calculator.setInput(digit.value)
+      updateDisplay()
     })
   })
 
   operators.forEach(operator => {
-    operator.addEventListener('click', (e: Event) => {
-      console.log(e.target)
-
-      calculator.setCurrent(+display.textContent)
+    operator.addEventListener('click', (e: Event): void => {
+      calculator.operation = operator.value
+      calculator.setPrevious(display.textContent)
+      calculator.setCurrent('')
+      calculate()
+      updateDisplay()
     })
   })
 
-  equals.addEventListener('click', (e: Event) => {
-    calculator.setCurrent(+display.textContent)
-    calculator.add()
+  equals.addEventListener('click', (): void => {
+    calculator.setCurrent(display.textContent)
+    console.log('Current', calculator.current)
+
+    calculate()
+    updateDisplay()
+  })
+
+  clearBtn.addEventListener('click', (): void => {
+    calculator.reset()
     updateDisplay()
   })
 })()

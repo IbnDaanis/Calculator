405a2bd313077ee97fdb2f44fa63c2027b79fd3b
Ikraam <IbnDaanis@Gmail.com>
2021-03-17 04:26:45 +0000

    cleared

--- src/index.ts
+++ src/index.ts
@@ -9,20 +9,7 @@ class Calculator {
     this.operation = null
   }
 
-  public setInput(input: string | number) {
-    const value =
-      this.current === '0' ? input.toString() : this.current.toString() + input.toString()
-    this.setCurrent(value)
-  }
-
-  public setCurrent(input: string | number) {
-    console.log('Input: ', input)
-    this.current = input.toString()
-  }
-
-  public setPrevious(input: string | number) {
-    this.previous = input.toString()
-  }
+  public setCurrent(input: string | number) {}
 
   public setOperation(input: string) {
     this.operation = input
@@ -60,59 +47,45 @@ const DOM_EVENTS = (() => {
   const percent = document.querySelector('[data-percent]')
   const display = document.querySelector('#displayText')
 
-  const updateDisplay = (): void => {
-    display.textContent = calculator.current.toString()
-  }
+  const updateDisplay = (): void => {}
 
   const calculate = (): void => {
-    console.log('Calculate', calculator.current, calculator.previous)
-    // switch (calculator.operation) {
-    //   case '/':
-    //     calculator.divide()
-    //     break
-    //   case '*':
-    //     calculator.multiply()
-    //     break
-    //   case '+':
-    //     calculator.add()
-    //     break
-    //   case '-':
-    //     calculator.subtract()
-    //     break
-    //   default:
-    //     return
-    // }
+    console.log(calculator.current, calculator.previous)
+    switch (calculator.operation) {
+      case '/':
+        calculator.divide()
+        break
+      case '*':
+        calculator.multiply()
+        break
+      case '+':
+        calculator.add()
+        break
+      case '-':
+        calculator.subtract()
+        break
+      default:
+        return
+    }
   }
 
   updateDisplay()
 
   digits.forEach(digit => {
-    digit.addEventListener('click', (e: Event) => {
-      calculator.setInput(digit.value)
-      updateDisplay()
-    })
+    digit.addEventListener('click', () => {})
   })
 
   operators.forEach(operator => {
-    operator.addEventListener('click', (e: Event): void => {
-      calculator.operation = operator.value
-      calculator.setPrevious(display.textContent)
-      calculator.setCurrent('')
-      calculate()
-      updateDisplay()
+    operator.addEventListener('click', (): void => {
+      calculator.setOperation(operator.value)
     })
   })
 
   equals.addEventListener('click', (): void => {
-    calculator.setCurrent(display.textContent)
-    console.log('Current', calculator.current)
-
     calculate()
-    updateDisplay()
   })
 
   clearBtn.addEventListener('click', (): void => {
     calculator.reset()
-    updateDisplay()
   })
 })()
